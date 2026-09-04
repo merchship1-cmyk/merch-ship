b466fae0d89b120514d075e51ce238e8e28391b3
merchship1-cmyk <merchship1@gmail.com>
2026-09-04 13:30:28 -0400

    ci: make Detox timeout configurable for software emulator fallback

--- v2/phase1a/mobile-sim/e2e/jest.config.cjs
+++ v2/phase1a/mobile-sim/e2e/jest.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
+const configuredTimeout = Number(process.env.ZENZY_DETOX_TEST_TIMEOUT_MS ?? 180000);
+
+if (!Number.isFinite(configuredTimeout) || configuredTimeout < 180000) {
+  throw new Error('ZENZY_DETOX_TEST_TIMEOUT_MS must be a finite number >= 180000.');
+}
+
 module.exports = {
   rootDir: '../../..',
   testMatch: ['<rootDir>/phase1a/mobile-sim/e2e/**/*.detox.js'],
-  testTimeout: 180000,
+  testTimeout: configuredTimeout,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
